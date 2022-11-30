0d3427c99280847dfec0b55e881f6a3d4422eaf1
Mizosu97 <earthpig97@gmail.com>
2022-11-30 20:42:36 +0000

    cta

---
+++ info.lua
@@ -0,0 +1,12 @@
+local info = {}
+
+info.pacman_depends = {"python-pip", "cairo", "gtk", "gobject-introspection", "desktop-file-utils", "xdg-utils", "xdg-user-dirs", "gtk-update-icon-cache", "shared-mime-info", "mesa-utils"}
+
+info.aur_depends = {}
+
+info.directories = {}
+
+info.files = {}
+
+return info
+
