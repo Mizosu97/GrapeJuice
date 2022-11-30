374c560336f2135476b43e2fc08a703a36938df5
Mizosu97 <97634559+Mizosu97@users.noreply.github.com>
2022-11-30 17:08:35 -0500

    Update info.lua

--- info.lua
+++ info.lua
@@ -6,7 +6,7 @@ info.aur_depends = {}
 
 info.directories = {"$HOME/grapejuice", "$HOME/.config/brinkervii", "$HOME/.local/share/grapejuice"}
 
-info.files = {"$HOME/.local/bin/grapejuice", "$HOME/.local/bin/grapejuice-gui"}
+info.files = {"$HOME/.local/bin/grapejuice", "$HOME/.local/bin/grapejuice-gui", "$HOME/.local/share/applications/grapejuice.desktop", "$HOME/.local/share/applications/roblox-app.desktop", "$HOME/.local/share/applications/roblox-player.desktop", "$HOME/.local/share/applications/roblox-studio.desktop", "$HOME/.local/share/applications/roblox-studio-auth.desktop"}
 
 return info
 
